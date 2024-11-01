511862f103941c9e7c2c58d6d9f0d48794e4d00c
CakeSystem <1625000148@qq.com>
2024-11-01 13:02:34 +0800

    4.1.4

--- Readme/65/install.sh
+++ Readme/65/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="4.1.3"
+VERSION="4.1.4"
 DOWNLOAD_HOST="https://github.com/CakeSystem/shortcut/raw/main/Readme/65/linux"
 ORIGIN_EXEC="qiangminer-${VERSION}"
 
@@ -346,6 +346,7 @@ ISSUE() {
     echo "3.9.94"
     echo "4.0.0"
     echo "4.1.3"
+    echo "4.1.4"
 }
 
 filterResult() {
