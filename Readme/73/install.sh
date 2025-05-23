5f7b2b4a3964ca5bbcbc012d8f709440b98f2338
CakeSystem <1625000148@qq.com>
2025-05-23 13:14:27 +0800

    4.4.8

--- Readme/73/install.sh
+++ Readme/73/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="4.4.7"
+VERSION="4.4.8"
 DOWNLOAD_HOST="https://github.com/CakeSystem/shortcut/raw/main/Readme/73/linux"
 ORIGIN_EXEC="goldminer-${VERSION}"
 
@@ -351,6 +351,7 @@ ISSUE() {
 	echo "4.4.3"
     echo "4.4.5"
     echo "4.4.7"
+    echo "4.4.8"
 }
 
 filterResult() {
